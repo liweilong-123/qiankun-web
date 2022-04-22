92ac33e78284116cdd8ac848511c8f458908a5d2
DESKTOP-NL5C28A <909616654@qq.com>
2022-04-22 13:16:02 +0800

    提交注释

---
+++ src/component/test.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Test {
+
+    constructor(){
+        this.xxxx.bind(this);
+    }
+
+    xxxx = () => {
+
+    }
+
+    render() {
+        return <div>xxxxxx</div>
+    }
+}
